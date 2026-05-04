9a5a2e71d420addf482d913ba61a0b0cafd9a7d8
robbatr0n <robbarton8@proton.me>
2026-05-04 12:48:04 +0100

    fixed build errors

--- api/game-players.ts
+++ api/game-players.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { sql } from './_db'
+import { sql } from './_db.js'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
